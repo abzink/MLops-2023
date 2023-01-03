cf0f9b42119fcdb83d6b02f7a4809053f49c6144
abzink <s213027@student.dtu.dk>
2023-01-03 10:22:57 +0100

    new branch

--- final_exercise/data.py
+++ final_exercise/data.py
@@ -8,6 +8,8 @@ def mnist():
     train_ims = torch.tensor(np.array([np.load(f"/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/train_{i}.npz")["images"] for i in range(5)]).reshape(-1,28,28))
     train_labels = torch.tensor(np.array([np.load(f"/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/train_{i}.npz")["labels"] for i in range(5)]).reshape(-1))
     
+    print('tiny change')
+    
     # exchange with the corrupted mnist dataset
     #train = torch.randn(50000, 784)
     #test = torch.randn(10000, 784) 
